7621e7679040c4c1fea94b437985d2a6fb67c1d6
jamie-codez <cruiseomondi90@gmail.com>
2023-07-03 23:37:36 +0300

    Cleaning up communications

--- src/pages/home/Communication.js
+++ src/pages/home/Communication.js
@@ -6,31 +6,22 @@ import {useEffect, useState} from "react";
 import Login from "../Login";
 
 const Communications = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    useEffect(() => {
-        const authenticated = localStorage.getItem("authenticated");
-        if (authenticated) {
-            setIsAuthenticated(true);
-        }
-    }, [isAuthenticated]);
     return (
-        <div>
-            {isAuthenticated ? <div className={"flex"}>
-                <SideBar/>
-                <div className={"flex flex-col w-full h-screen"}>
-                    <NavHeader/>
-                    <div className={"h-full w-full"}>
-                        <div className={"flex flex-col mt-10"}>
-                            <Table/>
-                        </div>
-                    </div>
-                    <div className={"align-baseline"}>
-                        <Footer/>
+        <div className={"flex"}>
+            <SideBar/>
+            <div className={"flex flex-col w-full h-screen"}>
+                <NavHeader/>
+                <div className={"h-full w-full"}>
+                    <div className={"flex flex-col mt-10"}>
+                        <Table/>
                     </div>
                 </div>
-            </div> : <Login/>}
+                <div className={"align-baseline"}>
+                    <Footer/>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Communications;
