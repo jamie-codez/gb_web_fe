8bda24dcfdbfc0ae28d001a4f0edc0f0dcf0d84f
jamie-codez <cruiseomondi90@gmail.com>
2023-07-03 23:34:42 +0300

    Cleaned up settings

--- src/pages/home/Settings.js
+++ src/pages/home/Settings.js
@@ -6,34 +6,23 @@ import {useEffect, useState} from "react";
 import Login from "../Login";
 
 const Settings = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    useEffect(() => {
-        const authenticated = localStorage.getItem("authenticated");
-        if (authenticated) {
-            setIsAuthenticated(true);
-        }
-    }, [isAuthenticated]);
     return (
-        <div>
-            {isAuthenticated ?
-                <div className={"flex"}>
-                    <SideBar/>
-                    <div className={"flex flex-col w-full h-screen"}>
-                        <NavHeader/>
-                        <div className={"h-full w-full"}>
-                            <div className={"center items-center h-full font-bold text-3xl"}>
-                                <h1>We are working on something great!! </h1>
-                                <h1>Thank you for being patient🙂</h1>
-                            </div>
-                        </div>
-                        <div className={"align-baseline bg-gray-300"}>
-                            <Footer/>
-                        </div>
+        <div className={"flex"}>
+            <SideBar/>
+            <div className={"flex flex-col w-full h-screen"}>
+                <NavHeader/>
+                <div className={"h-full w-full"}>
+                    <div className={"center items-center h-full font-bold text-3xl"}>
+                        <h1>We are working on something great!! </h1>
+                        <h1>Thank you for being patient🙂</h1>
                     </div>
-                </div> : <Login/>
-            }
+                </div>
+                <div className={"align-baseline bg-gray-300"}>
+                    <Footer/>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Settings;
