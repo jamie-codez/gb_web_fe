de9fbe1d626132afb2b4cbee05f842a65074e60a
jamie-codez <cruiseomondi90@gmail.com>
2023-07-07 00:22:17 +0300

    Callin payments

--- src/pages/home/Payments.js
+++ src/pages/home/Payments.js
@@ -1,21 +1,31 @@
 import SideBar from "../../components/SideBar";
 import NavHeader from "../../components/NavHeader";
 import Footer from "../../components/Footer";
-import {Table} from "./Dashboard";
+import { Table } from "./Dashboard";
+import { useState,useEffect } from "react";
+import axios from "axios";
 
 const Payments = () => {
+    const [payments, setPayments] = useState([]);
+    const getPayments = async () => {
+        const response = await axios.get("/api/payments");
+        setPayments(response.data);
+    }
+    useEffect(() => {
+        getPayments();
+    }, [payments, setPayments])
     return (
         <div className={"flex"}>
-            <SideBar/>
+            <SideBar />
             <div className={"flex flex-col w-full h-screen"}>
-                <NavHeader/>
+                <NavHeader />
                 <div className={"h-full w-full"}>
                     <div className={"flex flex-col w-full mt-10"}>
-                        <Table/>
+                        <Table />
                     </div>
                 </div>
                 <div className={"align-baseline"}>
-                    <Footer/>
+                    <Footer />
                 </div>
             </div>
         </div>
