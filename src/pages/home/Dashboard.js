bf8f381df1148538a86d74ebf6c27eaba2f7396e
jamie-codez <cruiseomondi90@gmail.com>
2023-07-07 06:44:47 +0300

    Created a row

--- src/pages/home/Dashboard.js
+++ src/pages/home/Dashboard.js
@@ -13,6 +13,35 @@ import { AiFillMessage, AiTwotoneHome } from "react-icons/ai";
 import { BiMoney, BiUser } from "react-icons/bi";
 
 
+export const Row = ({ data }) => {
+    return (
+        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <th scope="row"
+                className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                <img className="w-10 h-10 rounded-full" src={logo}
+                    alt="Jese" />
+                <div className="pl-3">
+                    <div className="text-base font-semibold">{data.username}</div>
+                    <div className="font-normal text-gray-500">{data.email}</div>
+                </div>
+            </th>
+            <td className="px-6 py-4">
+                {data.phone}
+            </td>
+            <td className="px-6 py-4">
+                <div className="flex items-center">
+                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
+                    {data.verified}
+                </div>
+            </td>
+            <td className="px-6 py-4">
+                <Link to={`dashboard/users/${data.email}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit
+                    user</Link>
+            </td>
+        </tr>
+    )
+}
+
 export const Table = () => {
     return (
         <div className={"table"}>
