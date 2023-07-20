ea5909446e78afa0a10d13559be13906bd5c25e4
jamie-codez <cruiseomondi90@gmail.com>
2023-07-20 18:48:28 +0300

    Fixing not a function error

--- src/pages/home/Houses.js
+++ src/pages/home/Houses.js
@@ -2,29 +2,33 @@ import SideBar from "../../components/SideBar";
 import NavHeader from "../../components/NavHeader";
 import Footer from "../../components/Footer";
 import {Table} from "./Dashboard";
-import {useEffect, useState} from "react";
-import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {useState, useEffect} from "react";
 
 const Houses = () => {
-    const navigate = useNavigate();
     const [houses, setHouses] = useState([]);
-    const getHouses = async () => {
-        const params = {
-            method: 'GET',
-            headers: {
-                "access-token": localStorage.getItem("accessToken"),
-                "Content-Type": "application/json"
+    
+    useEffect(() => {
+        const getHouses = async () => {
+            const params = {
+                method:"GET",
+                headers:{
+                    'Content-Type': 'application/json',
+                    'access-token': localStorage.getItem('accessToken')
+                }
+            }
+
+            const response = await fetch("http://localhost/houses/1",params);
+            if(response.status===200){
+                const data = response.json();
+                if(data.code===453){
+                    localStorage.clear();
+                    window.location.href="/login"
+                }else{
+                setHouses(data.payload.data);}
+            }else{
+                setHouses([])
             }
         }
-        const response = await fetch("http://localhost/houses/1", params);
-        const data = await response.json();
-        setHouses(data.payload.data);
-    }
-    const handleAddNewHouse = () => {
-        navigate("/dashboard/houses/new");
-    }
-    useEffect(() => {
         getHouses();
     }, [houses, setHouses]);
     return (
@@ -35,9 +39,7 @@ const Houses = () => {
                 <div className={"h-full w-full"}>
                     <div className={"flex flex-col w-full mt-10"}>
                         <div className={"flex flex-row mt-5 justify-end mr-20"}>
-                            <button className={"bg-purple-700 p-2 rounded-lg text-white mb-5"}
-                                    onClick={handleAddNewHouse}>Add New House
-                            </button>
+                            <button className={"bg-purple-700 p-2 rounded-lg text-white mb-5"}>Add New House</button>
                         </div>
                         <Table data={houses}/>
                     </div>
--- src/pages/home/Payments.js
+++ src/pages/home/Payments.js
@@ -1,9 +1,9 @@
 import SideBar from "../../components/SideBar";
 import NavHeader from "../../components/NavHeader";
 import Footer from "../../components/Footer";
-import {Table} from "./Dashboard";
-import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { Table } from "./Dashboard";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Payments = () => {
     const navigate = useNavigate();
@@ -20,8 +20,13 @@ const Payments = () => {
         const response = await fetch("http://localhost/payments/1", params);
         if (response.status === 200) {
             const data = await response.json();
-            setPayments(data.payload.data);
-        }else {
+            if (data.code === 453) {
+                localStorage.clear();
+                window.location.href = "/login"
+            } else {
+                setPayments(data.payload.data);
+            }
+        } else {
             setPayments([]);
         }
     }
@@ -48,21 +53,21 @@ const Payments = () => {
     }, [payments, setPayments])
     return (
         <div className={"flex"}>
-            <SideBar/>
+            <SideBar />
             <div className={"flex flex-col w-full h-screen"}>
-                <NavHeader/>
+                <NavHeader />
                 <div className={"h-full w-full"}>
                     <div className={"flex flex-col w-full mt-10"}>
                         <div className={"flex flex-row mt-5 justify-end mr-20"}>
                             <button className={"bg-purple-700 p-2 rounded-lg text-white mb-5"}
-                                    onClick={handleAddNewPaymentClick}>Add New Payment
+                                onClick={handleAddNewPaymentClick}>Add New Payment
                             </button>
                         </div>
-                        <Table data={payments} deleteCallback={handleDeletePayment}/>
+                        <Table data={payments} deleteCallback={handleDeletePayment} />
                     </div>
                 </div>
                 <div className={"align-baseline"}>
-                    <Footer/>
+                    <Footer />
                 </div>
             </div>
         </div>
