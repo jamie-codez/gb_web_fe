4d34197494c904ddd775a5ee93783c03172f5ee6
jamie-codez <cruiseomondi90@gmail.com>
2023-07-20 20:26:40 +0300

    Working on post and put

--- src/pages/home/Account.js
+++ src/pages/home/Account.js
@@ -3,33 +3,49 @@ import NavHeader from "../../components/NavHeader";
 import Footer from "../../components/Footer";
 import UserForm from "../../components/UserForm";
 import "../../index.css"
-import {useEffect, useState} from "react";
-import axios from "axios";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Account = () => {
-    const [user, setUsers] = useState({});
-    const client = axios.create({baseURL:"http://localhost"})
-    const getUser = async () => {
-        const response = await client.get("/users/1");
-        const data = response.data
-        setUsers(data.payload.data);
+    const [user, setUser] = useState(null);
+    const { id } = useParams();
+    if (id) {
+         getUser();
     }
-    useEffect(() => {
-        getUser();
-    }, [user, setUsers]);
+
+    const getUser = async () => {
+        const params = {
+            method: 'GET',
+            headers: {
+                'access-token': localStorage.getItem('accessToken'),
+                'Content-Type': 'application/json'
+            }
+        }
+        const response = await fetch(`http://localhost/users/${id}`, params);
+        if (response.status === 200) {
+            const jsonData = await response.json();
+            if (jsonData.payload.data.length === 0) {
+                alert("User not found");
+                return;
+            }
+            setUser(jsonData.payload.data);
+        }
+    };
+
     return (
         <div className={"flex"}>
-            <SideBar/>
+            <SideBar />
             <div className={"flex flex-col w-full h-screen max-h-full"}>
-                <NavHeader/>
+                <NavHeader />
                 <div className={"h-full mr-10 ml-10 mt-20"}>
-                    <UserForm/>
+                    <UserForm  />
                 </div>
                 <div className={"align-baseline"}>
-                    <Footer/>
+                    <Footer />
                 </div>
             </div>
-        </div>)
+        </div>
+    )
 }
 
 export default Account;
