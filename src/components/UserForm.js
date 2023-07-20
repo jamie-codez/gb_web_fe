0572b6c42555d100c90442395dd9376f01989aea
jamie-codez <cruiseomondi90@gmail.com>
2023-07-20 20:51:57 +0300

    POSTING and PUTTING

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,18 +1,56 @@
 import { useState } from "react";
 
 const UserForm = ({ userData }) => {
-    const [username,setUsername] = useState("");
-    const [email,setEmail] = useState("");
-    const [firstName,setFirstName] = useState("");
-    const [lastName,setLastName] = useState("");
-    const [phone,setPhone] = useState("");
-    const [password,setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [idNumber, setIdNumber] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password, setPassword] = useState("");
+
+    let verb;
+    if (userData) {
+        verb = "PUT";
+    } else {
+        verb = "POST";
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const params = {
+            method: verb,
+            headers: {
+                'access-token': localStorage.getItem('accessToken'),
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                email: email,
+                firstName: firstName,
+                lastName: lastName,
+                idNumber: idNumber,
+                profileImage: userData ? userData.profileImage : "",
+                phone: phone,
+                password: password
+            })
+        }
+        const response = await fetch(`http://localhost/users`, params);
+        if (response.status === 200) {
+            const jsonData = await response.json();
+            if (jsonData.payload.code === 200 || jsonData.payload.code === 201) {
+                alert("User created successfully");
+                return;
+            }
+            alert("User creation failed, try again");
+        }
+    }
 
     return (
         <div className={"account_form mt-10"}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="relative z-0 w-full mb-6 group">
-                    <input type="email" value={userData ? userData.name : ""} name="floating_email" id="floating_email"
+                    <input type="email" value={userData ? userData.email : ""} name="floating_email" id="floating_email" onChange={(e) => setEmail(e.target.value)}
                         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" " required />
                     <label htmlFor="floating_email"
@@ -20,23 +58,22 @@ const UserForm = ({ userData }) => {
                         address</label>
                 </div>
                 <div className="relative z-0 w-full mb-6 group">
-                    <input type="password" name="floating_password" id="floating_password"
+                    <input type="password" name="floating_password" id="floating_password" onChange={(e) => setPassword(e.target.value)}
                         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" " required />
                     <label htmlFor="floating_password"
                         className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 </div>
-                {/* <div className="relative z-0 w-full mb-6 group">
-                    <input type="password" name="repeat_password" id="floating_repeat_password"
+                <div className="relative z-0 w-full mb-6 group">
+                    <input type="text" name="repeat_password" id="floating_repeat_password" value={userData ? userData.idNumber : ""} onChange={(e) => setIdNumber(e.target.value)}
                         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" " required />
                     <label htmlFor="floating_repeat_password"
-                        className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm
-                        password</label>
-                </div> */}
+                        className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ID Number</label>
+                </div>
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 w-full mb-6 group">
-                        <input type="text" name="floating_first_name" id="floating_first_name"
+                        <input type="text" name="floating_first_name" id="floating_first_name" value={userData ? userData.firstName : ""} onChange={(e) => setFirstName(e.target.value)}
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" " required />
                         <label htmlFor="floating_first_name"
@@ -44,7 +81,7 @@ const UserForm = ({ userData }) => {
                             name</label>
                     </div>
                     <div className="relative z-0 w-full mb-6 group">
-                        <input type="text" name="floating_last_name" id="floating_last_name"
+                        <input type="text" name="floating_last_name" id="floating_last_name" value={userData ? userData.lastName : ""} onChange={(e) => setLastName(e.target.value)}
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" " required />
                         <label htmlFor="floating_last_name"
@@ -54,7 +91,7 @@ const UserForm = ({ userData }) => {
                 </div>
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 w-full mb-6 group">
-                        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone"
+                        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" value={userData ? userData.phone : ""} onChange={(e) => setPhone(e.target.value)}
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" " required />
                         <label htmlFor="floating_phone"
@@ -62,7 +99,7 @@ const UserForm = ({ userData }) => {
                             number (123-456-7890)</label>
                     </div>
                     <div className="relative z-0 w-full mb-6 group">
-                        <input type="text" name="floating_company" id="floating_company"
+                        <input type="text" name="floating_company" id="floating_company" value={userData ? userData.username : ""} onChange={(e) => setUsername(e.target.value)}
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" " required />
                         <label htmlFor="floating_company"
@@ -77,7 +114,7 @@ const UserForm = ({ userData }) => {
                             aria-describedby="user_avatar_help" id="user_avatar" type="file" />
                     </div>
                 </div>
-                <button type="submit" className="flex w-full btn justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <button type="submit" onClick={handleSubmit} className="flex w-full btn justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                     {userData ? "Update" : "Create"}
                 </button>
             </form>
