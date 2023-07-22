f2dbb77db2830800464627bfc951d3d35d5393cd
jamie-codez <cruiseomondi90@gmail.com>
2023-07-22 03:01:37 +0300

    On individuals

--- src/pages/home/CommunicationItemPage.js
+++ src/pages/home/CommunicationItemPage.js
@@ -2,31 +2,50 @@ import SideBar from "../../components/SideBar";
 import NavHeader from "../../components/NavHeader";
 import Footer from "../../components/Footer";
 import "../../index.css"
-import {useEffect, useState} from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
 import CommunicationForm from "../../components/CommunicationForm";
+import { useParams, useNavigate } from "react-router-dom";
 
 const CommunicationItemPage = () => {
-    const [user, setUsers] = useState({});
-    const client = axios.create({baseURL:"http://localhost"})
-    const getUser = async () => {
-        const response = await client.get("/communication/1");
-        const data = response.data
-        setUsers(data.payload.data);
+    const [communication, setCommunication] = useState({});
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const getCommunication = async (id) => {
+        const params = {
+            method: 'GET',
+            headers: {
+                "access-token": localStorage.getItem("accessToken"),
+                "Content-Type": "application/json"
+            }
+        };
+        const response = await fetch(`http://localhost/communications/${id}`, params);
+        if (response.status === 200) {
+            const data = await response.json();
+            if (data.status === 453) {
+                return navigate("/login");
+            }
+            setCommunication(data.payload);
+        } else {
+            setCommunication({});
+        }
     }
+
     useEffect(() => {
-        getUser();
-    }, [user, setUsers]);
+        if (id) {
+            getCommunication(id)
+        }
+    }, [communication, setCommunication]);
     return (
         <div className={"flex"}>
-            <SideBar/>
+            <SideBar />
             <div className={"flex flex-col w-full h-screen max-h-full"}>
-                <NavHeader/>
+                <NavHeader />
                 <div className={"h-full mr-10 ml-10 mt-20"}>
-                    <CommunicationForm/>
+                    <CommunicationForm data={communication} />
                 </div>
                 <div className={"align-baseline"}>
-                    <Footer/>
+                    <Footer />
                 </div>
             </div>
         </div>)
